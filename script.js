f7b8e703bafe198e4012ef0e52575c13b7d675af
Funmi Lawal <82683491+Funmi999@users.noreply.github.com>
2022-06-22 13:42:22 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var opr = prompt("operator:")
+var num1 = parseint(prompt("Number 1:"))
+var num2 = parseint(prompt("Number 2:"))
+
+if(opr == "+")
+{
+    console.log(num1 + num2)
+}
+else if (opr == "-")
+{
+    console.log(num1 - num2)
+}
+else if(opr == "/")
+{
+    console.log(num1 / num2)
+}
+else if (opr == "*")
+{
+    console.log(num1 * num2)
+}
